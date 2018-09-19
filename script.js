14eaa5d5dd5cc33668f52686594b0320c0b1f14c
Peter Farr <Peter@PrismaPhonic.com>
2018-09-18 18:10:55 -0700

    updated css

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 'use strict';
-$(document).ready(function() {
-  $('#search-dad-jokes').on('click', function() {
+$(document).ready(function () {
+  let $rowDads = $('#row-dads');
+  $('#search-dad-jokes').on('click', function () {
+    $rowDads.html('');
     let dadSearch = $('#search-dad-jokes').val();
     let dataObject = { limit: 8, term: dadSearch };
     $.get('https://icanhazdadjoke.com/search', dataObject, dadJokeGen, 'json');
     $('#dad-search').val('');
   });
   function dadJokeGen(res) {
-    let $rowDads = $('#row-dads');
     console.log(res.results);
-    for (let { id } of res.results) {
-      let t = `<img class="img-fluid" src="https://icanhazdadjoke.com/j/${id}.png"></img>`;
+    for (let { joke } of res.results) {
+      let t = `
+        <div class="col-3 m-3">
+          <p>${joke}</p>
+        </div>
+      `;
       $rowDads.append($(t));
     }
     //res.results.id
